2f40a928f99de1f38364e7a2410873dd4e99e946
TatsianaKalu <88622147+TatsianaKalu@users.noreply.github.com>
2021-10-12 00:50:21 +0300

    add functional and adaptive

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const schemeSvg = document.querySelector('.scheme-svg');
-const totalPriceTag = document.querySelector('.price-total')
+const totalPriceTag = document.querySelector('.price-total');
+const menuButton = document.querySelector('.m-menu');
+const menu = document.querySelector('.menu');
 let cost = 800;
 let totalPrice = 0;
 
@@ -15,5 +17,12 @@ schemeSvg.addEventListener('click', (event)=>{
 
     }
 });
+
+menuButton.addEventListener('click', () => {
+
+    menu.classList.toggle(".is-open")
+
+    }
+);
 
 
